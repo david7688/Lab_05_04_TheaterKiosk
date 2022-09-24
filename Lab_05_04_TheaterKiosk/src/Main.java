a5199229807a52f11f55901b7c7625cb2e6c4d38
david7688 <113467007+david7688@users.noreply.github.com>
2022-09-24 13:20:22 -0400

    Add files via upload

---
+++ Lab_05_04_TheaterKiosk/src/Main.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int age = 10;
+
+        if (age <=21);
+
+        {
+            System.out.println("You can get a wrist band.");
+        }
+
+    }
+}
